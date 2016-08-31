b511e4285ea08417a4f0798da4eeda8cc75d1bd8
robin2017 <robin.seu@foxmail.com>
2016-08-31 09:44:23 +0800

    ddd

---
+++ src/BinaryTree/Solution.java
@@ -0,0 +1,27 @@
+package BinaryTree;
+
+/**
+ * Created by robin on 16/8/31.
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args){
+        new Solution().invertTree(null);
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+
+        return null;
+    }
+}
